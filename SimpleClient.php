806881ce83939c0bfe4cb77e05d6fd1598159f10
CHAWIT WIRIYAKUN <chawitmild@gmail.com>
2019-10-13 20:00:00 +0700

    Remove Unuse Code

--- SimpleClient.php
+++ SimpleClient.php
@@ -3,7 +3,7 @@ $postData = file_get_contents("php://input");
 $json_data = json_decode($postData, true);
 $responseText = "Hello From php Server(You said ".$json_data["queryResult"]["queryText"].")"."<br>Response ID:".$json_data["responseId"];
 $response = array(
-    "fulfillmentText"=> $responseText,
+    "fulfillmentText"=> $responseText
     );
 echo json_encode($response);
 ?>
