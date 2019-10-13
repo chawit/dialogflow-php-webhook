d280a8514cbf5e5b9cfc94950cc5a01e8e43f2bf
CHAWIT WIRIYAKUN <chawitmild@gmail.com>
2019-10-13 19:52:54 +0700

    edit 5

--- SimpleClient.php
+++ SimpleClient.php
@@ -1,7 +1,7 @@
 <?php
 $postData = file_get_contents("php://input");
 $json_data = json_decode($postData, true);
-$responseText = "Hello From php Server(You said ".$json_data["queryResult"]["queryText"].")"."<br>Response ID:".$json_data["responseId"].
+$responseText = "Hello From php Server(You said ".$json_data["queryResult"]["queryText"].")"."<br>Response ID:".$json_data["responseId"];
 $response = array(
     "source" => "From Fulfillment",
     "fulfillmentText"=> $responseText,
