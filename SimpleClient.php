9ddc335627aebaad61c13bb1739aa691bae0ad55
CHAWIT WIRIYAKUN <chawitmild@gmail.com>
2019-10-13 17:48:33 +0700

    edit Simple Chat1

--- SimpleClient.php
+++ SimpleClient.php
@@ -2,7 +2,7 @@
 $postData = file_get_contents("php://input");
 $json_data = json_decode($postData, true);
 $response = array(
-    "source" => $update["responseId"],
+    "source" => "Form Fulfillment",
     "fulfillmentText"=>"Hello World",
     );
 echo json_encode($response);
